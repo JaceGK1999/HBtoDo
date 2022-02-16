acd3d980665ff45ccb5dcff1d06d55eed6e11ac3
Jacegk1999 <Jacegk1999@gmail.com>
2022-02-16 15:24:21 -0800

    added complete supabase funct

--- fetch-utils.js
+++ fetch-utils.js
@@ -36,6 +36,10 @@ export async function getTodos() {
 }
 
 export async function completeTodo(id) {
+    const response = await client.from('todos').update({ complete: true }).match({
+        user_id: client.auth.user().id,
+        id: id,
+    });
     // find the and update (set complete to true), the todo that matches the correct id
 
     return checkError(response);
