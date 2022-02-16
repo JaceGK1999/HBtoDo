dbc0fd3e9fcca206498aa77d50ebd61bd1238f0f
Jacegk1999 <Jacegk1999@gmail.com>
2022-02-16 15:32:31 -0800

    added form listener

--- todos/todos.js
+++ todos/todos.js
@@ -1,10 +1,10 @@
-import { 
-    checkAuth, 
-    createTodo, 
+import {
+    checkAuth,
+    createTodo,
     completeTodo,
     getTodos,
     logout,
-    deleteAllTodos, 
+    deleteAllTodos,
 } from '../fetch-utils.js';
 import { renderTodo } from '../render-utils.js';
 
@@ -15,17 +15,24 @@ const todoForm = document.querySelector('.todo-form');
 const logoutButton = document.querySelector('#logout');
 const deleteButton = document.querySelector('.delete-button');
 
-todoForm.addEventListener('submit', async(e) => {
+todoForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(todoForm);
+
+    const todo = formData.get('todo');
+
+    await createTodo(todo);
+
+    todoForm.reset();
+    displayTodos();
     // on submit, create a todo, reset the form, and display the todos
 });
 
 async function displayTodos() {
     // fetch the todos
-    
     // display the list of todos
-
     // be sure to give each todo an event listener
-
     // on click, complete that todo
 }
 
@@ -35,9 +42,7 @@ logoutButton.addEventListener('click', () => {
     logout();
 });
 
-
-deleteButton.addEventListener('click', async() => {
+deleteButton.addEventListener('click', async () => {
     // delete all todos
-
     // then refetch and display the updated list of todos
 });
